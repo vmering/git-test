9c51c968314c70283d670e326b262d4ab827805d
cccwm <1037014536@qq.com>
2021-12-12 22:05:05 +0800

    hot fix  second commit

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -4,5 +4,6 @@ public class git {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello gitttttttt");
+        System.out.println("hot fix change");
     }
 }
