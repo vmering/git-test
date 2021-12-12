0a84b644f495d8f0c421446b414be609d0425de3
cccwm <1037014536@qq.com>
2021-12-12 21:47:02 +0800

    second commit

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -1,5 +1,8 @@
 public class git {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
     }
 }
