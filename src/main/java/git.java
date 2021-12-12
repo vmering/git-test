29d02a3854c7fb891dbd2f27bdde927b4bbe4924
cccwm <1037014536@qq.com>
2021-12-12 22:27:49 +0800

    push commit

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -6,5 +6,6 @@ public class git {
         System.out.println("hello gitttttttt");
         System.out.println("master change");
         System.out.println("hot fix change");
+        System.out.println("push test");
     }
 }
