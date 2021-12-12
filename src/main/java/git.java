eb420ad79a0f8b935a56eaf40e379cc43732310c
helloman <75985842+vmering@users.noreply.github.com>
2021-12-12 22:31:55 +0800

    pull commit

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -7,5 +7,6 @@ public class git {
         System.out.println("master change");
         System.out.println("hot fix change");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
